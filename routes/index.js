c736cf7fda9658d5c728800cb5c32769b1ea9d08
Focrult <focrult@gmail.com>
2023-04-05 17:38:53 +0800

    removed excess comments

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,4 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
-// router.use((req, res) => {
-//   return res.send('Wrong route!');
-// });
-// router.get('/', (req, res) => {
-//   console.log('API is running!');
-//   res.json({ message: 'Welcome to my API!' });
-// });
 module.exports = router;
